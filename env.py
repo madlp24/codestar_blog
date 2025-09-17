d73df7a0374a3792d4284e4f04b46b12cda4930c
madlp24 <120478064+madlp24@users.noreply.github.com>
2025-09-17 17:20:43 -0500

    Deployment settings

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_xqn7Vw9fjueP@ep-muddy-river-agv7gk2y.c-2.eu-central-1.aws.neon.tech/glow_trade_bagel_595945"
+    )
