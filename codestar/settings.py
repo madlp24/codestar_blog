565b7b53cd11a105d29e8a2ac3a451d819c498e2
madlp24 <120478064+madlp24@users.noreply.github.com>
2025-09-17 19:31:09 -0500

    adds a Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,11 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-kugl%%uolo3dm8hf&nr=%v7i-kcz4@ll#qii9yg*4hw+jap(5a'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -81,7 +82,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- env.py
+++ env.py
@@ -3,3 +3,4 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_xqn7Vw9fjueP@ep-muddy-river-agv7gk2y.c-2.eu-central-1.aws.neon.tech/glow_trade_bagel_595945"
     )
+os.environ.setdefault("SECRET_KEY", "Monteoscuro")
