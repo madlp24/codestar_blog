c72be7f966992b6f22455bde83cfde511057e57e
madlp24 <120478064+madlp24@users.noreply.github.com>
2025-09-19 14:39:42 -0500

    Comment Class

--- blog/models.py
+++ blog/models.py
@@ -13,4 +13,13 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+class Comment(models.Model):
+    Post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
     
